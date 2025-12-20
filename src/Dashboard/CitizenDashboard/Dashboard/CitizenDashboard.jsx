39efd3ea3c4f34268b0cda030d98948456ea3ca2
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-12-20 13:33:45 +0600

    completed  citizen dashboard

--- src/Dashboard/CitizenDashboard/Dashboard/CitizenDashboard.jsx
+++ src/Dashboard/CitizenDashboard/Dashboard/CitizenDashboard.jsx
@@ -1,32 +1,80 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAxios from "../../../Hooks/useAxios";
+import Loading from "../../../SharedComponent/Loader/Loading";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 const CitizenDashboard = () => {
-    return (
-        <div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              <div className="card bg-base-200 shadow-md">
-                <div className="card-body">
-                  <h2 className="card-title">Total Issues</h2>
-                  <p className="text-3xl font-bold text-primary">124</p>
-                </div>
-              </div>
+  const axiosInstance = useAxios();
 
-              <div className="card bg-base-200 shadow-md">
-                <div className="card-body">
-                  <h2 className="card-title">Pending</h2>
-                  <p className="text-3xl font-bold text-warning">18</p>
-                </div>
-              </div>
+  const { data: stats = {}, isLoading } = useQuery({
+    queryKey: ["dashboardStats"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/citizendashboard-stats");
+      return res.data;
+    },
+  });
 
-              <div className="card bg-base-200 shadow-md">
-                <div className="card-body">
-                  <h2 className="card-title">Resolved</h2>
-                  <p className="text-3xl font-bold text-success">96</p>
-                </div>
-              </div>
-            </div>
+  const chartData = [
+    { name: "Pending", value: stats.pendingIssues },
+    { name: "In Progress", value: stats.inProgressIssues },
+    { name: "Resolved", value: stats.resolvedIssues },
+  ];
+
+  const COLORS = ["#FACC15", "#3B82F6", "#22C55E"];
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        <div className="stat bg-base-100 shadow rounded-xl">
+          <div className="stat-title">Total Issues</div>
+          <div className="stat-value">{stats.totalIssues}</div>
         </div>
-    );
+
+        <div className="stat bg-yellow-100 shadow rounded-xl">
+          <div className="stat-title">Pending</div>
+          <div className="stat-value">{stats.pendingIssues}</div>
+        </div>
+
+        <div className="stat bg-blue-100 shadow rounded-xl">
+          <div className="stat-title">In Progress</div>
+          <div className="stat-value">{stats.inProgressIssues}</div>
+        </div>
+
+        <div className="stat bg-green-100 shadow rounded-xl">
+          <div className="stat-title">Resolved</div>
+          <div className="stat-value">{stats.resolvedIssues}</div>
+        </div>
+
+        <div className="stat bg-purple-100 shadow rounded-xl">
+          <div className="stat-title">Total Payments</div>
+          <div className="stat-value">৳ {stats.totalPayments}</div>
+        </div>
+      </div>
+      <div className="max-w-100 mt-5 mx-auto bg-base-100 shadow rounded-xl p-4">
+        <PieChart width={400} height={300}>
+        <Pie
+          data={chartData}
+          cx="50%"
+          cy="50%"
+          outerRadius={100}
+          dataKey="value"
+          label
+        >
+          {chartData.map((_, index) => (
+            <Cell key={index} fill={COLORS[index]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+      </div>
+    </>
+  );
 };
 
 export default CitizenDashboard;
