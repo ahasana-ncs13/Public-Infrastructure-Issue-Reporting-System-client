2bb93f81c5112640bcdbe387c8f92d16fec998bd
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-12-22 14:58:04 +0600

    completed token verification

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,12 +1,45 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
-const axioInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000",
 });
 
 const useAxios = () => {
-    return axioInstance
+  const navigate=useNavigate()
+  const { user,signOutUser } = useAuth();
+  useEffect(() => {
+    const reqInterceptor = axiosInstance.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    const resInterceptor = axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+        const statusCode=error.status
+        if (statusCode===401||statusCode===403 ) {
+          signOutUser()
+          .then(()=>{
+            navigate("/loginlayout/login")
+          })
+        }
+
+        return Promise.reject(error)
+      }
+    );
+
+    return () => {
+      axiosInstance.interceptors.request.eject(reqInterceptor);
+      axiosInstance.interceptors.response.eject(resInterceptor);
+    };
+  }, [user,signOutUser,navigate]);
+  return axiosInstance;
 };
 
 export default useAxios;
